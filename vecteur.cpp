4bd5a3ddc7d59a9089e7c5f68b4fc015f3e5dabd
Zupanico <50524719+Zupanico@users.noreply.github.com>
2024-02-08 22:42:14 -0500

    feat:vecteur

--- vecteur.cpp
+++ vecteur.cpp
@@ -9,16 +9,18 @@
 #include "vecteur.h"
 #include <stdexcept>
 
-template <typename T>
+using namespace std;
+
+template <class T>
 Vecteur<T>::Vecteur() : head(nullptr), size(0) {}
 
-template <typename T>
+template <class T>
 Vecteur<T>::~Vecteur() {
     clear();
 }
 
 
-template <typename T>
+template <class T>
 T& Vecteur<T>::at(int index) {
     if (index >= size || head == nullptr) {
         throw std::out_of_range("Index out of bounds");
@@ -36,17 +38,17 @@ T& Vecteur<T>::at(int index) {
 
 }
 
-template <typename T>
+template <class T>
 int Vecteur<T>::getSize() const {
     return size;
 }
 
-template <typename T>
+template <class T>
 bool Vecteur<T>::isEmpty() const {
     return size == 0;
 }
 
-template <typename T>
+template <class T>
 void Vecteur<T>::clear() {
     while (head != nullptr) {
         Node<T>* temp = head;
@@ -56,7 +58,7 @@ void Vecteur<T>::clear() {
     size = 0;
 }
 
-template <typename T>
+template <class T>
 T& Vecteur<T>::removeAt(int index) {
     if (index >= size || head == nullptr) {
         throw std::out_of_range("Index out of bounds");
@@ -82,7 +84,7 @@ T& Vecteur<T>::removeAt(int index) {
 
 }
 
-template <typename T>
+template <class T>
 void Vecteur<T>::append(const T& value) {
     Node<T>* newNode = new Node<T>(value);
 
@@ -99,4 +101,101 @@ void Vecteur<T>::append(const T& value) {
     size++;
 }
 
+// implémentation de l'opérateur << en ostream
+template <typename U>
+std::ostream &operator<<(std::ostream &out, const Vecteur<U> &v)
+{
+    Node<U> *current = v.head;
+    while (current != nullptr)
+    {
+        out << current->data << " ";
+        current = current->next;
+    }
+    return out;
+}
+
+// implémentation de l'opérateur << en ofstream
+template <typename U>
+std::ofstream &operator<<(std::ofstream &ofs, const Vecteur<U> &v)
+{
+    Node<U> *current = v.head;
+    while (current != nullptr)
+    {
+        ofs << current->data << " ";
+        current = current->next;
+    }
+    return ofs;
+}
+
+template <typename U>
+std::ostringstream &operator<<(std::ostringstream &oss, const Vecteur<U> &v)
+{
+    Node<U> *current = v.head;
+    while (current != nullptr)
+    {
+        oss << current->data << " ";
+        current = current->next;
+    }
+    return oss;
+}
+
+// implémentation de l'opérateur >> en istream
+template <typename T>
+std::istream &operator>>(std::istream &is, Vecteur<T> &v)
+{
+    T value;
+    while (is >> value)
+    {
+        v.append(value);
+    }
+    return is;
+}
+
+template <typename U>
+std::ifstream &operator>>(std::ifstream &ifs, Vecteur<U> &v)
+{
+    U value;
+    while (ifs >> value)
+    {
+        v.append(value);
+    }
+    return ifs;
+}
+
+template <typename U>
+std::istringstream &operator>>(std::istringstream &iss, Vecteur<U> &v)
+{
+    U value;
+    while (iss >> value)
+    {
+        v.append(value);
+    }
+    return iss;
+}
+
+template <class T>
+void Vecteur<T>::operator++()
+{   // Augmente l'index courant de 1
+    ++currentIndex;
+}
+
+template <class T>
+void Vecteur<T>::operator--()
+{   // Decremente l'index courant de 1
+    --currentIndex;
+}
+
+template <typename T>
+void Vecteur<T>::operator+=(const T& value)
+{
+    this->append(value);
+}
+
+template <typename T>
+T &Vecteur<T>::operator[](int index)
+{
+    return this->at(index);
+}
+
 #endif
+
--- vecteur.h
+++ vecteur.h
@@ -8,20 +8,95 @@
 #ifndef _VECTEUR_H
 #define _VECTEUR_H
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 template <typename T>
 class Node {
 public:
     T data;
     Node* next;
 
+    // opérateur pour << ostream
+    template <typename U>
+    friend std::ostream &operator<<(std::ostream &os, const Node<U> &n);
+
+    // opérateur pour << ofstream
+    template <typename U>
+    friend std::ofstream &operator<<(std::ofstream &ofs, const Node<U> &v);
+
+    // opérateur pour << ostringstream
+    template <typename U>
+    friend std::ostringstream &operator<<(std::ostringstream &oss, const Node<U> &v);
+ 
+    // opérateur pour >> istream
+    template <typename U>
+    friend std::istream &operator>>(std::istream &is, Node<U> &n);
+
+    // opérateur pour >> ifstream
+    template <typename U>
+    friend std::ifstream &operator>>(std::ifstream &ifs, Node<U> &n);
+
+    // opérateur pour >> ostringstream
+    template <typename U>
+    friend std::istringstream &operator>>(std::istringstream &iss, Node<U> &n);
+    
+
     Node(const T& newData) : data(newData), next(nullptr) {}
 };
+
+// opérateur pour << ostream
+template <typename U>
+inline std::ostream &operator<<(std::ostream &os, const Node<U> &n)
+{
+    os << n.data;
+    return os;
+}
+
+// opérateur pour << ofstream
+template <typename U>
+inline std::ofstream &operator<<(std::ofstream &ofs, const Node<U> &n)
+{
+    ofs << n.data;
+    return ofs;
+}
+
+// opérateur pour << ostringstream
+template <typename U>
+inline std::ostringstream &operator<<(std::ostringstream &oss, const Node<U> &n){
+    oss << n.data;
+    return oss;
+}
+
+// opérateur pour >> istream
+template <typename U>
+inline std::istream &operator>>(std::istream &is, Node<U> &n)
+{
+    is >> n.data;
+    return is;
+}
+
+template <typename U>
+inline std::ifstream &operator>>(std::ifstream &ifs, Node<U> &n)
+{
+    ifs >> n.data;
+    return ifs;
+}
+
+template <typename U>
+inline std::istringstream &operator>>(std::istringstream &iss, Node<U> &n)
+{
+    iss >> n.data;
+    return iss;
+}
 
 template <typename T>
 class Vecteur {
 private:
     Node<T>* head;
     int size;
+    int currentIndex = 0;
 
 public:
     Vecteur();
@@ -39,6 +114,43 @@ public:
     T& removeAt(int index);
 
     void append(const T& value);
+
+    //Opérateurs
+
+    // <<
+    template <typename U>
+    friend std::ostream &operator<<(std::ostream &out, const Vecteur<U> &v);
+
+    template <typename U>
+    friend std::ofstream &operator<<(std::ofstream &ofs, const Vecteur<U> &v);
+
+    template <typename U>
+    friend std::ostringstream &operator<<(std::ostringstream &oss, const Vecteur<U> &v);
+
+    // >>
+    template <typename U>
+    friend std::istream &operator>>(std::istream &is, Vecteur<U> &v);
+
+    template <typename U>
+    friend std::ifstream &operator>>(std::ifstream &ifs, Vecteur<U> &v);
+
+    template <typename U>
+    friend std::istringstream &operator>>(std::istringstream &iss, Vecteur<U> &v);
+
+
+    // ++
+    void operator++();
+
+    // --
+    void operator--();
+
+    // +=
+    void operator+=(const T& value);
+
+    // []
+    T& operator[](int index);
 };
+
+#include "vecteur.cpp"
 
 #endif // _VECTOR_H
